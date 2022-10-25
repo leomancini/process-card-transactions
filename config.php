0fd2540ed2083fd86325cd9275ec8c65b38351c5
Leo Mancini <mancini@leo.gd>
2022-10-24 23:44:14 -0400

    Updating name of data file

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 	$CONFIG = [
-		'DATA_FILE' => '../data/todaysTransactions.txt',
+		'DATA_FILE' => '../data/transactions.txt',
 		'EMAIL_SENDER' => '"Card Bot" <leo@leo.local>',
 		'EMAIL_RECIPIENT' => 'card-transactions@leo.gd',
 		'DEBUG_MODE' => false

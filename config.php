4e9e45891291f1ab582d777bd8877fa52983e151
Leo Mancini <mancini@leo.gd>
2022-10-24 23:38:43 -0400

    Updating sender name

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 	$CONFIG = [
 		'DATA_FILE' => '../data/todaysTransactions.txt',
-		'EMAIL_SENDER' => 'Card Bot <leo@leo.local>',
+		'EMAIL_SENDER' => '"Card Bot" <leo@leo.local>',
 		'EMAIL_RECIPIENT' => 'card-transactions@leo.gd',
 		'DEBUG_MODE' => false
 	];
